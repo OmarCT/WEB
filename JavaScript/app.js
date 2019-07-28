86eddb9670b0683d17ae520444fd02a2e0cbfee4
Omar Celis Torres <omarcelist@gmail.com>
2019-07-27 23:46:48 -0500

    COmmit inicial: Subiendo archivos de prueba .js y .html

---
+++ JavaScript/app.js
@@ -0,0 +1,32 @@
+document.write("<h1>Hola Mundo!</h1>");
+
+// Tipos de datos
+
+// Tipo String
+"Hola Mundo"
+'Adios Mundo'
+
+// number
+1
+2.3
+-2
+
+// boolean
+true
+false
+
+// array
+
+['Omar', 'Celis', 'Torres', 28, true]
+
+// objeto
+
+{
+    "nombre": 'Omar',
+    "apellido": 'Celis',
+    "edad": 28,
+    "vivo": true
+}
+
+
+
